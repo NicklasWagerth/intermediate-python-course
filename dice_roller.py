ef4a4f4a4dfc6ef9e7e57e074732ee11c5f6c166
NicklasWagerth <nwagerth@cisco.com>
2022-01-24 14:28:59 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,12 @@
 #This is like taking the first walk on the moon
 import random
+dice_rolls = 2
 def main():
-    roll = random.randint(1,6)
-  #print('You rolled a die and feel like a frikn King!')
-    print(f'You rolled a {roll}')
+    dice_sum = 0
+    for i in range(0, dice_rolls):
+        roll = random.randint(1,6)
+        print(f'You rolled a {roll}')
+        dice_sum += roll
+    print(f'You have rolled a total sum of {dice_sum}')
 if __name__== "__main__":
   main()
