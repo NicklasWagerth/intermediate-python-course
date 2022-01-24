b11c4f18630fd6ba80a05e9c771150c295cfde2b
NicklasWagerth <nwagerth@cisco.com>
2022-01-24 14:04:02 +0100

    I created a comment and added some text

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,6 @@
+#This is like taking the first walk on the moon
 def main():
-  print('You rolled a die')
+  print('You rolled a die and feel like a frikn King!')
 
 if __name__== "__main__":
   main()
